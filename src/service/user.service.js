8179f900ce3440a22ffc095aadbb7a8a460e2304
CYZ99 <86727104+CYZ99@users.noreply.github.com>
2023-06-13 14:31:56 +0800

    fix: 增加昵称字段

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -2,10 +2,14 @@ const connection = require('../app/database');
 
 class UserService {
   async create(user) {
-		const { username, password } = user;
-		const statement = 'INSERT INTO `users` (name, password) VALUES (?, ?);';
+		const { username, password, nickname } = user;
+		const statement = 'INSERT INTO `users` (name, password, nickname) VALUES (?, ?, ?);';
 		// 3. 执行 sql 语句
-		const [result] = await connection.execute(statement, [username, password]);
+		const [result] = await connection.execute(statement, [
+			username,
+			password,
+			nickname
+		]);
 		return result;
 	}
 	async findUserName(name) {
